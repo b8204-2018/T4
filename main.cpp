c40b5dd019f6b43098b7e89e09579fbd5d6f5ce5
linaverde <p.koroleva1102@gmail.com>
2018-12-19 21:28:42 +1000

    Create deck structure for integers

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "src/circleDeck.h"
+
+int main() {
+    std::cout << "В программе ничего нет, но тут должны быть тесты" << std::endl;
+    return 0;
+}
---
+++ src/circleDeck.cpp
@@ -0,0 +1,126 @@
+//
+// Created by polina on 19.12.18.
+//
+
+#include <iostream>
+#include "circleDeck.h"
+
+#define ERR -1;
+
+const char CREATE_ERROR[] = "Количество элементов дека должно быть натуральным числом";
+const char IS_FULL[] = "Дек переполнен";
+const char IS_EMPTY[] = "Дек пуст";
+
+const int defSize = 10;
+
+myDeck::myDeck() {
+    size = defSize;
+    deck = new int [size];
+    front = 5;
+    back = 5;
+};
+
+myDeck::myDeck(int n) {
+    try {
+        if (n < 1) {
+            throw ERR;
+        }
+        size = n;
+        deck = new int[size];
+        front = size/2;
+        back = size/2;
+    }
+    catch (int i) {
+        std::cout << CREATE_ERROR << std:: endl;
+        size = 0;
+        front = 0;
+        back = 0;
+        deck = nullptr;
+    }
+
+};
+
+void myDeck::reInit(int n) {
+    try {
+        if (n < 1) {
+            throw ERR;
+        }
+        size = n;
+        deck = new int[size];
+        front = size/2;
+        back = size/2;
+    }
+    catch (int i) {
+        std::cout << CREATE_ERROR << std:: endl;
+        size = 0;
+        front = 0;
+        back = 0;
+        deck = nullptr;
+    }
+    size = n;
+    if (deck != nullptr){
+        delete [] deck;
+    }
+    deck = new int[size];
+    front = size/2;
+    back = size/2;
+};
+
+void myDeck::push_front(int elem) {
+    if (front == 0) {
+        front = size;
+    }
+    try {
+        if (front - 1 == back) {
+            throw ERR;
+        }
+        deck[--front] = elem;
+    }
+    catch (int i) {
+        std::cout << IS_FULL << std:: endl;
+    }
+};
+
+void myDeck::push_back(int elem) {
+    if (back == size - 1) {
+        back = -1;
+    }
+    try {
+        if (back + 1 == front) {
+            throw ERR;
+        }
+        deck[++back] = elem;
+    }
+    catch (int i) {
+        std::cout << IS_FULL << std:: endl;
+    }
+};
+
+int myDeck::pop_front() {
+    try {
+        if (front == back) {
+            throw ERR;
+        }
+        return deck[front++];
+    }
+    catch (char *str) {
+        std::cout << IS_EMPTY << std:: endl;
+    }
+};
+
+int myDeck::pop_back() {
+    try {
+        if (front == back) {
+            throw ERR;
+        }
+        return deck[back--];
+    }
+    catch (char *str) {
+        std::cout << IS_EMPTY << std:: endl;
+    }
+};
+
+bool myDeck::empty() {
+    return (front == back);
+};
+
