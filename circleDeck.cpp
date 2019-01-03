321e8fdabc2be19dd6dfec112f6137f04d0b27c9
linaverde <p.koroleva1102@gmail.com>
2019-01-04 00:06:37 +1000

    Delete files; add all test cases; add tests; figure out a problem and stop

--- circleDeck.cpp
+++ circleDeck.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 #include "circleDeck.h"
 
-#define CREATE_ERROR "Количество элементов дека должно быть натуральным числом";
-#define IS_FULL  "Дек переполнен";
-#define IS_EMPTY "Дек пуст";
+#define CREATE_ERROR "Deck's size must be an positive integer";
+#define IS_FULL  "Deck is full";
+#define IS_EMPTY "Deck is empty";
 
 const int defSize = 10;
 
@@ -31,25 +31,30 @@ myDeck::myDeck(int n) {
 };
 
 
-void myDeck::push_front(int elem) {
-    if (front == 0) {
-        front = size;
-    }
+void myDeck::push_front(int elem) { //front указывает на ячейку, в которой уже есть элемент
     if (front - 1 == back) {
         throw IS_FULL;
     }
+
+    if (front == 0) {
+        front = size;
+    }
+
     deck[--front] = elem;
 
 };
 
-void myDeck::push_back(int elem) {
-    if (back == size - 1) {
-        back = -1;
-    }
+void myDeck::push_back(int elem) { //back указывает на пустую ячейку. В предыдущей ячейке есть элемент
     if (back + 1 == front) {
         throw IS_FULL;
+    }о
+    if (back + 1 == size) {
+        back = 0;
+        deck[size - 1] = elem;
     }
-    deck[++back] = elem;
+    else{
+        deck[back++] = elem;
+    }
 
 };
 
@@ -57,17 +62,22 @@ int myDeck::pop_front() {
     if (front == back) {
         throw IS_EMPTY;
     }
+    if (front + 1 == size) {
+        int i = front;
+        front = 0;
+        return deck[i];
+    }
     return deck[front++];
-
 };
 
 int myDeck::pop_back() {
     if (front == back) {
         throw IS_EMPTY;
     }
-    return deck[back--];
-
-
+    if (back - 1 < 0){
+        back = size;
+    }
+    return deck[--back];
 };
 
 bool myDeck::empty() {
@@ -81,17 +91,22 @@ void myDeck::print() {
             i = 0;
         }
     }
+    std::cout << std::endl;
 };
 
 int myDeck::length() {
-    int count = 0;
-    for (int i = front; i == back + 1; i++) {
-        count++;
-        i++;
-        if (i == size) {
-            i = 0;
+    if (front == back) {
+        return 0;
+    } else {
+        int count = 0;
+        for (int i = front; i != back; i++) {
+            if (i == size) {
+                i = -1;
+                count--;
+            }
+            count++;
         }
+        return count;
     }
-    return count;
 }
 
--- tests.cpp
+++ tests.cpp
@@ -1,18 +1,157 @@
 #include <iostream>
+#include <cstring>
 #include "catch.hpp"
 #include "circleDeck.h"
 
-TEST_CASE("Deck is alive", "[deck]"){
+TEST_CASE("Deck is alive", "[deck]") {
 
-    myDeck a;
-    myDeck b(5);
-
-    SECTION("Deck is succesfully created"){
-        REQUIRE( a.size == 10 );
-        REQUIRE( a.length() == 0 );
-        REQUIRE( b.size == 5);
-        REQUIRE( b.length() == 0);
+    SECTION("Deck is succesfully created") {
+        myDeck a; //default constructor
+        REQUIRE(a.size == 10);
+        REQUIRE(a.length() == 0);
+        REQUIRE(a.empty());
+        myDeck b(5); //custom constructor
+        REQUIRE(b.size == 5);
+        REQUIRE(b.length() == 0);
+        REQUIRE(a.empty());
     }
-    
+
+}
+
+TEST_CASE("Pushing and popping elements from front", "[deck]") {
+
+    SECTION("One element") {
+        myDeck a;
+        a.push_front(1);
+        CHECK(a.size == 10);
+        CHECK(!a.empty());
+        CHECK(a.length() == 1);
+        CHECK(a.pop_front() == 1);
+        CHECK(a.size == 10);
+        CHECK(a.empty());
+        CHECK(a.length() == 0);
+    }
+
+    SECTION("Some elements") {
+        myDeck a;
+        a.push_front(1);
+        a.push_front(2);
+        a.push_front(3);
+        CHECK(!a.empty());
+        CHECK(a.length() == 3);
+        CHECK(a.pop_front() == 3);
+        CHECK(a.length() == 2);
+        CHECK(a.pop_front() == 2);
+        CHECK(a.length() == 1);
+        CHECK(a.pop_front() == 1);
+        CHECK(a.empty());
+    }
+
+    SECTION("Circle") {
+        myDeck a(5);
+        a.push_front(1);
+        a.push_front(2);
+        a.push_front(3);
+        a.push_front(4);
+        CHECK(a.length() == 4);
+        CHECK(a.pop_front() == 4);
+        CHECK(a.pop_front() == 3);
+        CHECK(a.length() == 2);
+    }
+
+}
+
+TEST_CASE("Pushing and popping elements from back", "[deck]") {
+
+    SECTION("One element") {
+        myDeck a;
+        a.push_back(1);
+        CHECK(a.size == 10);
+        CHECK(!a.empty());
+        CHECK(a.length() == 1);
+        CHECK(a.pop_back() == 1);
+        CHECK(a.size == 10);
+        CHECK(a.empty());
+        CHECK(a.length() == 0);
+    }
+
+    SECTION("Some elements") {
+        myDeck a;
+        a.push_back(1);
+        a.push_back(2);
+        a.push_back(3);
+        CHECK(!a.empty());
+        CHECK(a.length() == 3);
+        CHECK(a.pop_back() == 3);
+        CHECK(a.length() == 2);
+        CHECK(a.pop_back() == 2);
+        CHECK(a.length() == 1);
+        CHECK(a.pop_back() == 1);
+        CHECK(a.empty());
+    }
+
+    SECTION("Circle") {
+        myDeck a(5);
+        a.push_back(1);
+        a.push_back(2);
+        a.push_back(3);
+        a.push_back(4);
+        CHECK(a.length() == 4);
+        CHECK(a.pop_back() == 4);
+        CHECK(a.pop_back() == 3);
+        CHECK(a.length() == 2);
+    }
+
+}
+
+TEST_CASE("Exceptions", "[deck]") {
+
+    SECTION("Initialize") {
+
+        try {
+            myDeck a(0);
+        }
+        catch (const char* str) {
+            REQUIRE(strcmp(str, "Deck's size must be an positive integer") == 0);
+        }
+
+        try {
+            myDeck a(-5);
+        }
+        catch (const char* str) {
+            REQUIRE(strcmp(str, "Deck's size must be an positive integer") == 0);
+        }
+
+    }
+
+    SECTION("Empty") {
+        myDeck a;
+        REQUIRE_THROWS_WITH(a.pop_front(), "Deck is empty");
+        REQUIRE_THROWS_WITH(a.pop_back(), "Deck is empty");
+    }
+
+    /*SECTION("Full"){
+        myDeck a(3);
+        a.push_back(0);
+        a.push_back(0);
+        a.push_front(0);
+        REQUIRE_THROWS_WITH(a.push_front(0), "Deck is full");
+        REQUIRE_THROWS_WITH(a.push_back(0), "Deck is full");
+    }*/
+
+}
+
+TEST_CASE("Printing", "[deck]"){
+    SECTION("Empty deck"){
+
+    }
+
+    SECTION("Some elements"){
+
+    }
+
+    SECTION("Full deck"){
+
+    }
 
 }
