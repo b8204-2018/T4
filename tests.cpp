2468683cb520034375de5063322f8c37f767481a
Nastbekkerman <nastbekkerman@mail.ru>
2019-01-19 15:06:05 +1000

    test

--- tests.cpp
+++ tests.cpp
@@ -1,50 +1,11 @@
-#include "base64.hpp"
+#include "list_queue_libs .h"
 #include "gtest/gtest.h"
 
-TEST(Encode, threeLetters) {
-char *s = base64_encode((const char*)"ups");
-EXPECT_STREQ(s, "dXBz");
+
+TEST(Length, emptyList) {
+queue *list;
+list->init();
+int l = list->lenghtS();
+EXPECT_EQ(l, 0);
 }
 
-
-TEST(Encode, fourLetters) {
-char *s = base64_encode((const char*)"oops");
-EXPECT_STREQ(s, "b29wcw==");
-}
-
-TEST(Encode, fiveLetters) {
-char *s = base64_encode((const char*)"param");
-EXPECT_STREQ(s, "cGFyYW0=");
-}
-
-
-TEST(Encode, empty) {
-char *s = base64_encode((const char*)"");
-EXPECT_STREQ(s, "");
-}
-
-TEST(Decode, noEqualSigns) {
-char *s = base64_decode((const char*)"ZG9n");
-EXPECT_STREQ(s, "dog");
-}
-
-
-TEST(Decode, twoEqualSigns) {
-char *s = base64_decode((const char*)"ZG9nZw==");
-EXPECT_STREQ(s, "dogg");
-}
-
-TEST(Decode, empty) {
-char *s = base64_decode((const char*)"");
-EXPECT_STREQ(s, "");
-}
-
-TEST(Decode, oneEqualSign) {
-char *s = base64_decode((const char*)"ZG9nIGc=");
-EXPECT_STREQ(s, "dog g");
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
